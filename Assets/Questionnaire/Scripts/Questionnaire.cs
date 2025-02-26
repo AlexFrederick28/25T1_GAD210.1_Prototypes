cc286a973153e71758f6f5024fb774ba3a789258
AlexFrederick28 <alexanderfrederick12@hotmail.com>
2025-02-26 17:36:05 +1100

    Added prefabs of creatures as well as questionnaire functionality

---
+++ Assets/Questionnaire/Scripts/Questionnaire.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Security.Cryptography.X509Certificates;
+
+public class Questionnaire : MonoBehaviour
+{
+    public TextMeshProUGUI creatureText;
+
+    public TextMeshProUGUI locationText;
+
+    public GameObject[] creatureToSpawn;
+
+    public List<string> creatureChosen = new List<string>();
+
+    public List<string> locationChosen = new List<string>();
+
+    public string answerScene = "Answer";
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveAnswers();
+
+            SwitchScene();
+
+            SpawnAnswers();
+        }
+    }
+
+    public void SaveAnswers()
+    {
+        creatureChosen.Add(creatureText.text);
+
+        locationChosen.Add(locationText.text);
+
+    }
+
+    public void SwitchScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Questionnaire")
+        {
+            SceneManager.LoadScene(answerScene);  
+        }
+    }
+
+    public void SpawnAnswers()
+    {
+        if (SceneManager.GetActiveScene().name == answerScene)
+        {
+            if (creatureChosen.Contains("Shark"))
+            {
+                Instantiate(creatureToSpawn[0]);
+            }
+            else if (creatureChosen.Contains("Lion"))
+            {
+                Instantiate(creatureToSpawn[1]);
+            }
+            else if (creatureChosen.Contains("Military AI Robot"))
+            {
+                Instantiate(creatureToSpawn[2]);
+            }
+
+            if (locationChosen.Contains("500m Above Ground"))
+            {
+
+            }
+            else if (locationChosen.Contains("500m Below Ground"))
+            {
+
+            }
+            else if (locationChosen.Contains("Open Ocean"))
+            {
+
+            }
+
+        }
+    }
+}
